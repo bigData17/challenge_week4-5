35d50e34191fb3dd2e95b12b2c8fcac3e06283b8
bigData17 <32248874+bigData17@users.noreply.github.com>
2017-10-09 18:49:31 +0200

    Create pulling_xmlsequentially.py

---
+++ pulling_xmlsequentially.py
@@ -0,0 +1,55 @@
+from lxml import etree
+
+
+infile = 'split1.xml'
+context = etree.iterparse(infile, events=('end',), tag=('title', 'ns', 'id', 'text'))
+
+titles = open('titles.txt', 'w')
+namespaces = open('namespaces.txt', 'w')
+articles = open('articles.txt', 'w')
+ids = open('ids.txt', 'w')
+for event, elem in context:
+    
+    #Adding page number
+    #if i%7 == 0:
+    #    out.write('%d\n' % int(i/7) )
+        
+    #Adding title
+    if elem.tag == 'title':
+        if not elem.text.encode('utf-8'):
+            titles.write('ERROR: No Title Content Found\n' )
+        if elem.text.encode('utf-8'):
+            titles.write('%s\n' % elem.text.encode('utf-8'))
+    
+    #Adding namespace
+    if elem.tag == 'ns':
+        if not elem.text.encode('utf-8'):
+            namespaces.write('ERROR: No Namespace Content Found\n' )
+        if elem.text.encode('utf-8'):
+            namespaces.write('%s\n' % elem.text.encode('utf-8'))
+    
+    #Adding ID
+    if elem.tag == 'id':
+        if not elem.text.encode('utf-8'):
+            ids.write('ERROR: No ID Content Found\n' )
+        if elem.text.encode('utf-8'):
+            ids.write('%s\n' % elem.text.encode('utf-8'))
+            
+    #Adding title
+    if elem.tag == 'text':
+        if not elem.text.encode('utf-8'):
+            articles.write('ERROR: No Text Content Found\n' )
+        if elem.text.encode('utf-8'):
+            articles.write('%s\n' % elem.text.encode('utf-8'))
+     
+    # Making it memory efficient to avoid clogging RAM    
+    #deleting descendants
+    elem.clear()
+    #deleting now-empty references from the root node to tag <title>, etc 
+    while elem.getprevious() is not None:
+        del elem.getparent()[0]
+del context
+titles.close()
+namespaces.close()
+articles.close()
+ids.close()
